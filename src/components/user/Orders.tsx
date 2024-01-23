e3ae5854771b4d3404fbf9179d1698708b76d827
Mohammed Ahsan C <muhammedahsanc22@gmail.com>
2024-01-23 19:08:43 +0530

    fix-some-warnings

--- src/components/user/Orders.tsx
+++ src/components/user/Orders.tsx
@@ -1,63 +1,63 @@
 import * as React from 'react';
 import Link from '@mui/material/Link';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell from '@mui/material/TableCell';
+// import TableHead from '@mui/material/TableHead';
+// import TableRow from '@mui/material/TableRow';
 import Title from './Title';
 
 // Generate Order Data
-function createData(
-  id: number,
-  date: string,
-  name: string,
-  shipTo: string,
-  paymentMethod: string,
-  amount: number,
-) {
-  return { id, date, name, shipTo, paymentMethod, amount };
-}
+// function createData(
+//   id: number,
+//   date: string,
+//   name: string,
+//   shipTo: string,
+//   paymentMethod: string,
+//   amount: number,
+// ) {
+//   return { id, date, name, shipTo, paymentMethod, amount };
+// }
 
-const rows = [
-  createData(
-    0,
-    '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
-  ),
-  createData(
-    1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
-  ),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(
-    3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
-  ),
-];
+// const rows = [
+//   createData(
+//     0,
+//     '16 Mar, 2019',
+//     'Elvis Presley',
+//     'Tupelo, MS',
+//     'VISA ⠀•••• 3719',
+//     312.44,
+//   ),
+//   createData(
+//     1,
+//     '16 Mar, 2019',
+//     'Paul McCartney',
+//     'London, UK',
+//     'VISA ⠀•••• 2574',
+//     866.99,
+//   ),
+//   createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
+//   createData(
+//     3,
+//     '16 Mar, 2019',
+//     'Michael Jackson',
+//     'Gary, IN',
+//     'AMEX ⠀•••• 2000',
+//     654.39,
+//   ),
+//   createData(
+//     4,
+//     '15 Mar, 2019',
+//     'Bruce Springsteen',
+//     'Long Branch, NJ',
+//     'VISA ⠀•••• 5919',
+//     212.79,
+//   ),
+// ];
 
-function preventDefault(event: React.MouseEvent) {
-  event.preventDefault();
-}
+// function preventDefault(event: React.MouseEvent) {
+//   event.preventDefault();
+// }
 
 export default function Orders() {
   return (
@@ -85,7 +85,7 @@ export default function Orders() {
           ))}
         </TableBody>
       </Table> */}
-      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+      <Link color="primary" href="#"  sx={{ mt: 3 }}>
         See more orders
       </Link>
     </React.Fragment>
