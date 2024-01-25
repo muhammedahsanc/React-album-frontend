66c5fbac921c26453a5744798e0e7efccceb1442
Mohammed Ahsan C <muhammedahsanc22@gmail.com>
2024-01-25 17:11:18 +0530

    google-rejoined-issue-solved

--- src/components/functions/googleSubmit.ts
+++ src/components/functions/googleSubmit.ts
@@ -5,16 +5,15 @@ import { notify, errorToast } from "../../utils";
 const googleSubmit = async(email:string)=> {
 
     try{
-        if(email){
-            
+        if(email){   
         const data =  await axios.post("/addGoogleLoginData/googleLogin", {email});
         const token = data.data.token
-        Cookies.set('token',token,{ expires: 840000, secure: true })
+        Cookies.set('token',JSON.stringify(token))
         notify(data.data.message)
         return data
         }else{
             throw new Error("Something went wrong");
-    }
+        }
     
     }catch(error:any){
         console.error('An error occurred:', error);
--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://mygallery-backend.onrender.com/",
+  baseURL: "http://localhost:4000/",
 });
 
 export default instance;
+// baseURL: "https://mygallery-backend.onrender.com/",
