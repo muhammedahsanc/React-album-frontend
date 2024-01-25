af3008a2a8afad96be4a5ffcdc84e28e9718a10f
Mohammed Ahsan C <muhammedahsanc22@gmail.com>
2024-01-25 17:15:29 +0530

    backed-link-change

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:4000/",
+  baseURL:  "https://mygallery-backend.onrender.com/",
 });
 
 export default instance;
-// baseURL: "https://mygallery-backend.onrender.com/",
